bf36c410ad79ea219df89f4b1b637c61be1ce9af
mvienneau <michael.vienneau@colorado.edu>
2022-04-04 13:03:06 -0600

    :pray:

---
+++ entrypoint.sh
@@ -0,0 +1,59 @@
+#!/bin/sh
+
+set -e
+set -x
+
+if [ -z "$INPUT_SOURCE_FOLDER" ]
+then
+  echo "Source folder must be defined"
+  return -1
+fi
+
+if [ $INPUT_DESTINATION_HEAD_BRANCH == "main" ] || [ $INPUT_DESTINATION_HEAD_BRANCH == "master"]
+then
+  echo "Destination head branch cannot be 'main' nor 'master'"
+  return -1
+fi
+
+if [ -z "$INPUT_PULL_REQUEST_REVIEWERS" ]
+then
+  PULL_REQUEST_REVIEWERS=$INPUT_PULL_REQUEST_REVIEWERS
+else
+  PULL_REQUEST_REVIEWERS='-r '$INPUT_PULL_REQUEST_REVIEWERS
+fi
+
+CLONE_DIR=$(mktemp -d)
+
+echo "Setting git variables"
+export GITHUB_TOKEN=$API_TOKEN_GITHUB
+git config --global user.email "$INPUT_USER_EMAIL"
+git config --global user.name "$INPUT_USER_NAME"
+
+echo "Cloning destination git repository"
+git clone "https://$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+
+echo "Copying contents to git repo"
+mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER/
+cp $INPUT_SOURCE_FOLDER "$CLONE_DIR/$INPUT_DESTINATION_FOLDER/"
+cd "$CLONE_DIR"
+git checkout -b "$INPUT_DESTINATION_HEAD_BRANCH"
+
+
+echo "Creating Branch"
+git checkout -b $INPUT_DESTINATION_HEAD_BRANCH
+git branch -u origin $INPUT_DESTINATION_HEAD_BRANCH
+echo "Adding git commit"
+git add docs/output/
+if git status | grep -q "Changes to be committed"
+then
+  git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
+  echo "Pushing git commit"
+  git push
+  echo "Creating a pull request"
+  gh pr create -t "Auto PR from moov-node changes" \
+               -b "Syncing change from moov-node to docs" \
+               -B "master" \
+               -H $INPUT_DESTINATION_HEAD_BRANCH 
+else
+  echo "No changes detected"
+fi
