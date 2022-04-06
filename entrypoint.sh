8e1bbd8687d36fb921b28242b8176460973469a2
mvienneau <michael.vienneau@colorado.edu>
2022-04-06 14:46:19 -0600

    building

--- entrypoint.sh
+++ entrypoint.sh
@@ -33,19 +33,21 @@ git config --global user.name "$INPUT_USER_NAME"
 
 echo "Cloning destination git repository"
 git clone "https://$INPUT_USER_NAME:$API_TOKEN_GITHUB@github.com/$INPUT_DESTINATION_REPO.git" "$CLONE_DIR"
+cd "$CLONE_DIR"
+
+echo "Creating Branch if not exist"
+git checkout $INPUT_DESTINATION_HEAD_BRANCH || git checkout -b $INPUT_DESTINATION_HEAD_BRANCH
+git pull origin $INPUT_DESTINATION_HEAD_BRANCH || echo "New branch, no pull needed"
+
 
 echo "Copying contents to git repo"
 mkdir -p $CLONE_DIR/$INPUT_DESTINATION_FOLDER
 cp -r $INPUT_SOURCE_FOLDER "${CLONE_DIR}/${INPUT_DESTINATION_FOLDER}"
-cd "$CLONE_DIR"
 
 
-echo "Creating Branch if not exist"
-git checkout $INPUT_DESTINATION_HEAD_BRANCH || git checkout -b $INPUT_DESTINATION_HEAD_BRANCH
-# git checkout -b $INPUT_DESTINATION_HEAD_BRANCH
 echo "Adding git commit"
 git add content/node/
-if git status | grep -q "Changes to be committed"
+if (git status | grep -oq "content/node/")
 then
   git commit --message "Update from https://github.com/$GITHUB_REPOSITORY/commit/$GITHUB_SHA"
   echo "Pushing git commit"
